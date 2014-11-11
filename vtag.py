a11acd41fe9d2a44e9be9a0f91cb4d9831096bd7
Akshay Srivatsan <asrivat1@ugrad10.cs.jhu.edu>
2014-11-10 20:30:24 -0500

    Done with Problem 4

--- vtag.py
+++ vtag.py
@@ -14,7 +14,8 @@ vocab = defaultdict(int)
 tag_dict = defaultdict(list)
 all_tags = {}
 sing_tt = defaultdict(int)
-sing_tw = defaultdict(int)
+sing_wt = defaultdict(int)
+n = int()
 
 # Find the counts from test data
 def train(fileName):
@@ -39,10 +40,11 @@ def train(fileName):
             tag_dict[line[0]].append(line[1])
            
         # Also update singleton counts appropriately
-        if count_tt["/".join((line[0], line[1]))] == 1:
-            sing_tt[line[t_old]] += 1
-        elif count_tt["/".join((line[0], line[1]))] == 2:
-            sing_tt[line[t_old]] -= 1
+        if t_old is not None:
+            if count_tt["/".join((line[1], t_old))] == 1:
+                sing_tt[t_old] += 1
+            elif count_tt["/".join((line[1], t_old))] == 2:
+                sing_tt[t_old] -= 1
 
         if count_wt["/".join((line[0], line[1]))] == 1:
             sing_wt[line[1]] += 1
@@ -61,6 +63,8 @@ def test(fileName):
     # Read file into memory
     data = file(fileName, "r")
     data = data.readlines()
+    global n
+    n = len(data) - 1
 
     # Initialize things
     mu = defaultdict(lambda: float('-inf'), {})
@@ -106,18 +110,18 @@ def test(fileName):
             continue
         # Increment correct words and total
         if actualTags[i] == t[i]:
-            if w[i] not in vocab:
+            if vocab[w[i]] == 0:
                 novelCorrect += 1
             else:
                 knownCorrect += 1
-        if w[i] not in vocab:
+        if vocab[w[i]] == 0:
             novelTotal += 1
         else:
             knownTotal += 1
 
     # Print the results
-    for i in range(0, len(t)):
-        print "%s/%s" % (w[i], t[i])
+    '''for i in range(0, len(t)):
+        print "%s/%s" % (w[i], t[i])'''
     # If no novel words encountered, we are vacuously 100% accuracy
     print "Tagging accuracy (Viterbi decoding): %.2f%% (known: %.2f%% novel: %.2f%%)" % \
           (100 * (novelCorrect + knownCorrect) / (novelTotal + knownTotal),
@@ -125,20 +129,22 @@ def test(fileName):
     print "Perplexity per Viterbi-tagged test word: %.3f" % math.exp(- totalProb / (len(w) - 1))
 
 def p_tt(t1, t2):
-    '''
-    numerator = count_tt["/".join((t1, t2))]
-    denominator = count[t2]
-    return math.log(float(numerator + 1) / float(denominator + 1 + len(all_tags.keys())))
-    '''
-    numerator = count_tt["/".join((t1, t2))] + (1 + sing_tt(t1)) * p_tt_b(t2, t1)
-    denominator = count[t1] + 1 + sing_tt(t1)
+    numerator = count_tt["/".join((t1, t2))] + (1 + sing_tt[t1]) * p_tt_b(t1, t2)
+    denominator = count[t1] + 1 + sing_tt[t1]
+    return math.log(float(numerator) / float(denominator))
 
-def p_wt(w_i, t):
-    if w_i == "###" and t == "###":
+def p_tt_b(t1, t2):
+    return float(count[t1]) / float(n)
+
+def p_wt(w, t):
+    if w == "###" and t == "###":
         return 0
-    numerator = count_wt["/".join((w_i, t))]
-    denominator = count[t]
-    return math.log(float(numerator + 1) / float(denominator + len(vocab)))
+    numerator = count_wt["/".join((w, t))] + (1 + sing_wt[t]) * p_wt_b(w, t)
+    denominator = count[t] + 1 + sing_wt[t]
+    return math.log(float(numerator) / float(denominator))
+
+def p_wt_b(w, t):
+    return float(vocab[w] + 1) / float(n + len(vocab) + 1)
 
 def main():
     train(sys.argv[1])
